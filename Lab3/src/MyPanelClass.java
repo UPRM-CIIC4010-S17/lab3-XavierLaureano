bdae17acd88cd79fea0fdd0b724fbcd817bec6ef
Xavier <Xavier@Xavier>
2017-11-16 00:12:45 -0400

    Deleted commented lines
    Added white lines 
    
    Changed the star position
    
    Created a blue triangle 
    
    Completed the flag

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -1,15 +1,19 @@
 import java.awt.Color;
+
 import java.awt.Graphics;
+
 import java.awt.Insets;
+import java.awt.Polygon;
+
 import javax.swing.JPanel;
+
  
 public class MyPanelClass extends JPanel {
-
     private static final long serialVersionUID = 7057541440811488699L;
-
             public void paintComponent(Graphics g) {
+
                         super.paintComponent(g);
- 
+
                         //Compute interior coordinates
                         Insets myInsets = this.getInsets();
                         int x1 = myInsets.left;
@@ -18,25 +22,36 @@ public class MyPanelClass extends JPanel {
                         int y2 = getHeight() - myInsets.bottom - 1;
                         int width = x2 - x1;
                         int height = y2 - y1;
- 
-                        //Paint the background
-                        g.setColor(Color.BLACK);
-                        g.fillRect(x1, y1, width + 1, height + 1);
-//                        //Draw a border
-//                        g.setColor(Color.YELLOW);
-//                        g.drawRect(x1, y1, width, height);    
-//                        //Draw a second border
-//                        g.setColor(Color.CYAN);
-//                        g.drawRect(x1+10, y1+10, width-20, height-20);    
-//                        //Line 1
-//                        g.setColor(Color.WHITE);
-//                        g.drawLine(x1, y1, x2, y2);
-//                        //Line 2
-//                        g.setColor(Color.BLUE);
-//                        g.drawLine(x1, y2, x2, y1);
-                        g.setColor(Color.GREEN);
-                        g.fillOval((width/2)-27, (height/2)-27, width = 55, height = 55);
                         
+//                        Paint the background
+                          g.setColor(Color.RED);
+                          g.fillRect(x1, y1, width + 1, height + 1);
                        
+                          g.setColor(Color.WHITE);
+                          g.fillRect(0,50,width,50);
+                          g.setColor(Color.WHITE);
+                          g.fillRect(0,150,width,50);
+                          
+                          Polygon p3 = new Polygon();
+                          p3.addPoint(x1, y1);
+                          p3.addPoint(x1+160, y1+125);
+                          p3.addPoint(x1, y1+240);
+                          g.setColor(Color.BLUE);
+                          g.fillPolygon(p3);
+            
+                          Polygon p2 = new Polygon();
+                          p2.addPoint(x1 + 25, y1 + 113);
+                          p2.addPoint(x1 + 41, y1 + 113);
+                          p2.addPoint(x1 + 47, y1 + 98);
+                          p2.addPoint(x1 + 53, y1 + 113);
+                          p2.addPoint(x1 + 69, y1 + 113);
+                          p2.addPoint(x1 + 56, y1 + 123);
+                          p2.addPoint(x1 + 61, y1 + 138);
+                          p2.addPoint(x1 + 47, y1 + 128);
+                          p2.addPoint(x1 + 34, y1 + 138);
+                          p2.addPoint(x1 + 38, y1 + 123);
+                          g.setColor(Color.WHITE);
+                          g.fillPolygon(p2);
             }
+
 }
